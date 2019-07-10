9f5b2bb9d6a2ce9fc7e010c7106a5d074bf7f2df
Patrick <pwcarney@bu.edu>
2019-07-10 19:45:09 -0400

    Added text mesh pro text box

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    public TextMeshProUGUI text_field;
+
+    void Start()
+    {
+        string text = System.IO.File.ReadAllText(Application.dataPath + "/StreamingAssets/chapter1.txt");
+        string[] text_split = text.Split(new[] { "\n" }, StringSplitOptions.RemoveEmptyEntries);
+        foreach (string line in text_split)
+        {
+            if (line != "")
+            {
+                text_field.text = line;
+            }
+        }
+
+        StartCoroutine(RevealCharacters());
+    }
+
+    IEnumerator RevealCharacters()
+    {
+        text_field.ForceMeshUpdate();
+
+        TMP_TextInfo textInfo = text_field.textInfo;
+
+        int totalVisibleCharacters = textInfo.characterCount; 
+        int visibleCount = 0;
+
+        while (true)
+        {
+            text_field.maxVisibleCharacters = visibleCount;
+
+            visibleCount += 1;
+
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+}
